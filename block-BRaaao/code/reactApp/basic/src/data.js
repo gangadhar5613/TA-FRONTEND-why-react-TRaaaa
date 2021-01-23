121f89068b5652d9649a1002999f690f651ead1f
Narapureddy Gangadhar Reddy <gangadhar.5613@gmail.com>
2021-01-23 14:52:08 +0530

    Complete layout using create-react-app

---
+++ block-BRaaao/code/reactApp/basic/src/data.js
@@ -0,0 +1,28 @@
+const data= [
+    {
+        imageURL:'https://images.unsplash.com/photo-1594309161197-b676ce294b84?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjJ8fGhpbWFsYXlhfGVufDB8MHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        title:'The Journey to Himachal'
+    },
+    {
+        imageURL:'https://images.unsplash.com/photo-1604153352266-ac3a5fc876a5?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDR8fGhpbWFsYXlhfGVufDB8MHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        title:'Seeing Himalayas from AltCampus'
+    },
+    {
+        imageURL:'https://images.unsplash.com/photo-1568641276595-cd4dae028707?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mzh8fGhpbWFsYXlhfGVufDB8MHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        title:'Seeing big mountains and enjoying the nature'
+    },
+    {
+        imageURL:'https://images.unsplash.com/photo-1589800463007-3be49fe18b92?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MzJ8fGhpbWFsYXlhfGVufDB8MHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        title:'Seeing Himalayas firs time'
+    },
+    {
+        imageURL:'https://images.unsplash.com/photo-1600774841769-e7ac3da8fb99?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mjd8fGhpbWFsYXlhfGVufDB8MHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        title:'First time trekking'
+    },
+    {
+        imageURL:'https://images.unsplash.com/photo-1561567668-3716831ac029?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTl8fGhpbWFsYXlhfGVufDB8MHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        title:'Trekking Journey'
+    }
+]
+
+export default data;
