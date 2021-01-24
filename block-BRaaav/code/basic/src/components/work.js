4b3bfd4b3019b2428b3db3bd160bbdb443b3acf6
Narapureddy Gangadhar Reddy <gangadhar.5613@gmail.com>
2021-01-25 00:37:08 +0530

    Convert HTML to React

---
+++ block-BRaaav/code/basic/src/components/work.js
@@ -0,0 +1,51 @@
+function Work(){
+    return (
+        <>
+                 <section class="work padding">
+        <div class="container">
+          <header class="sec-header text-center">
+            <h2 class="heading">Our Work</h2>
+            <div class="dot-wrapper">
+              <div class="dot"></div>
+              <div class="dot"></div>
+              <div class="dot"></div>
+            </div>
+          </header>
+          <div class="flex">
+            <div class="flex-23">
+              <img
+                class="flexible-img"
+                src="/images/work-image1.jpg"
+                alt="Work"
+              />
+            </div>
+            <div class="flex-23">
+              <img
+                class="flexible-img"
+                src="/images/work-image2.jpg"
+                alt="Work"
+              />
+            </div>
+            <div class="flex-23">
+              <img
+                class="flexible-img"
+                src="/images/work-image3.jpg"
+                alt="Work"
+              />
+            </div>
+            <div class="flex-23">
+              <img
+                class="flexible-img"
+                src="/images/work-image4.jpg"
+                alt="Work"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+        </>
+    )
+}
+
+
+export default Work;
